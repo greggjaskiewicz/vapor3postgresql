4b8f16490e9226a4c8fd06c3783fccc06f019794
Gregg Jaskiewicz <gryzman@gmail.com>
2019-03-17 17:48:04 +0000

    postgresql

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,11 +1,8 @@
-import FluentSQLite
+import FluentPostgreSQL
 import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
-    // Register providers first
-    try services.register(FluentSQLiteProvider())
-
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
@@ -17,16 +14,16 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     middlewares.use(ErrorMiddleware.self) // Catches errors and converts to HTTP response
     services.register(middlewares)
 
-    // Configure a SQLite database
-    let sqlite = try SQLiteDatabase(storage: .memory)
+    let config = PostgreSQLDatabaseConfig(hostname: "localhost", port: 5432, username: "postgres", database: "SwiftTodo", password: nil, transport: .cleartext)
+    let postgres = PostgreSQLDatabase(config: config)
 
-    // Register the configured SQLite database to the database config.
+    /// Register the configured PostgreSQL database to the database config.
     var databases = DatabasesConfig()
-    databases.add(database: sqlite, as: .sqlite)
+    databases.add(database: postgres, as: .psql)
     services.register(databases)
 
-    // Configure migrations
+    /// Configure migrations
     var migrations = MigrationConfig()
-    migrations.add(model: Todo.self, database: .sqlite)
+    migrations.add(model: Todo.self, database: .psql)
     services.register(migrations)
 }
