be529f53f5d23ee6b7c9fc31f9936f79b556c895
greg jaskiewicz <gryzman@gmail.com>
2019-03-18 18:01:17 +0000

    fix crash

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -3,6 +3,9 @@ import Vapor
 
 /// Called before your application initializes.
 public func configure(_ config: inout Config, _ env: inout Environment, _ services: inout Services) throws {
+    
+    try services.register(FluentPostgreSQLProvider()) 
+   
     // Register routes to the router
     let router = EngineRouter.default()
     try routes(router)
